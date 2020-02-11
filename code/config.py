f1e4c16a1dd1c0d4524ec57d8e3350f99e70751e
Eao-Kind <a2393754@gmail.com>
2020-02-12 00:47:03 +0800

    first commit

---
+++ code/config.py
@@ -0,0 +1,75 @@
+import os
+import re
+
+
+# 需要签到的网站
+neednamelist = ['btschool']
+
+
+headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gec、'
+                      'ko) Chrome/79.0.3945.130 Safari/537.36', }
+
+
+# 请求验证码的url，只有需要url_img的网址才会用到这个
+checkin_php = {
+    'HDSky': 'https://hdsky.me/image_code_ajax.php',
+    'OpenCD': 'https://www.open.cd/plugin_sign-in.php', }
+
+
+# 获取验证码hash的表单数据，只有需要url_img的网址才会用到这个
+date = {
+    'HDSky': '{"action": "new"}',
+    'OpenCD': '1', }
+
+
+# 提交验证码url，只有需要url_img的网址才会用到这个
+post_submit = {
+    'HDSky': 'https://hdsky.me/showup.php',
+    'OpenCD':'https://www.open.cd/plugin_sign-in.php?cmd=signin', }
+
+
+# 用于组合图片地址
+url_img = {
+    'HDSky': '',
+}
+
+
+# 用于签到
+signin_ok = {
+    'btschool': 'http://pt.btschool.club/index.php?action=addbonus',
+}
+
+
+# 验证码识别所需配置
+NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+ALL_CHAR_SET = NUMBER + ALPHABET
+ALL_CHAR_SET_LEN = len(ALL_CHAR_SET)
+MAX_CAPTCHA = 6
+
+
+#处理cookies
+def cookies(web_name):
+    with open(web_name, 'r') as f:
+        lnum, data = 0, '#LWP-Cookies-2.0\n'   # 从饼干获取到的格式头不对，此处进行替换
+        temp = f.readlines()  # 由于只能使用一次读行，所有要设计一个变量保存读到的内容， 列表
+        if temp[0] == "// Semicolon separated Cookie File\n":  # 判断是否未处理的文本:
+            for line in temp[4:]:
+                #print(line)
+                data += line
+            temp = re.sub('expires="(.*?)"', 'expires="2038-01-19 03:14:07Z"', data)  # 替换expires内容
+            f.close()
+            with open(web_name, 'w') as f:
+                f.write(temp)
+                f.close()
+                print("{}cookie已处理".format(web_name))
+
+
+if __name__ == '__main__':
+    os.chdir('../cookies')
+    file = os.listdir()
+    print(file)
+    for text in file:
+        if text[-3:] == 'txt':
+            cookies(text)
---
+++ code/predict.py
@@ -0,0 +1,70 @@
+from PIL import Image
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torchvision.transforms as transforms
+import numpy as np
+from config import *
+
+
+# 处理图片
+transforms = transforms.Compose([
+        #transforms.Grayscale(), #灰度
+        transforms.ToTensor(),  # range [0, 255] -> [0.0,1.0]，自动每个数/255
+        #transforms.Normalize(mean=[0.5], std=[0.5])#单通道
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+    ])
+
+
+class VGG(nn.Module):
+    def __init__(self):
+        super(VGG, self).__init__()
+        self.features = self._make_layers([64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'])
+        self.classifier = nn.Sequential(
+            nn.Linear(9216, MAX_CAPTCHA*ALL_CHAR_SET_LEN)
+        )
+
+    def forward(self, x):
+        out = self.features(x)
+        out = out.view(out.size(0), -1)
+        out = self.classifier(out)
+        return out
+
+    def _make_layers(self, cfg):
+        layers = []
+        in_channels = 3
+        for x in cfg:
+            if x == 'M':
+
+                layers += [nn.MaxPool2d(kernel_size=2, stride=2, padding=1)]
+            else:
+                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
+                           nn.BatchNorm2d(x),
+                           nn.ReLU(inplace=True)]
+                in_channels = x
+        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
+        return nn.Sequential(*layers)
+
+
+def predict(image):
+    my_cnn = VGG() #加载网络
+    if torch.cuda.is_available():
+        my_cnn.cuda()
+    my_cnn.load_state_dict(torch.load('16pt.pth'))  # 加载训练好的模型
+    image = transforms(image)  # 这里经过转换后输出的input格式是[C,H,W],网络输入还需要增加一维批量大小B
+    image = image.unsqueeze(0)  # 增加一维，输出的img格式为[1,C,H,W]
+    vimage = Variable(image).cuda()  # 转换一下 才可以反向,预测
+    predict_label = my_cnn(vimage)
+    c0 = ALL_CHAR_SET[np.argmax(predict_label[0, 0:  ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    c1 = ALL_CHAR_SET[np.argmax(predict_label[0, 1 * ALL_CHAR_SET_LEN:2 * ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    c2 = ALL_CHAR_SET[np.argmax(predict_label[0, 2 * ALL_CHAR_SET_LEN:3 * ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    c3 = ALL_CHAR_SET[np.argmax(predict_label[0, 3 * ALL_CHAR_SET_LEN:4 * ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    c4 = ALL_CHAR_SET[np.argmax(predict_label[0, 4 * ALL_CHAR_SET_LEN:5 * ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    c5 = ALL_CHAR_SET[np.argmax(predict_label[0, 5 * ALL_CHAR_SET_LEN:6 * ALL_CHAR_SET_LEN].data.cpu().numpy())]
+    predict_label = '%s%s%s%s%s%s' % (c0, c1, c2, c3, c4, c5)
+    return predict_label
+
+
+if __name__ == '__main__':
+    img = Image.open('temp.png')
+    print(predict(img))
